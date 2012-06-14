683a42d56befa32c5a1d9ac1fa60494cd27579b7
dai <dai@daimatz.net>
2012-06-14 18:21:00 +0900

    prefix oj

--- base.lisp
+++ base.lisp
@@ -1,42 +1,42 @@
 (ql:quickload :drakma)
 (ql:quickload :s-http-client)
 
-(defclass OJ ()
-     ((id :accessor id
+(defclass oj ()
+     ((id :accessor oj-id
           :initarg :id)
-      (user :accessor user
+      (user :accessor oj-user
             :initarg :user)
-      (pass :accessor pass
+      (pass :accessor oj-pass
             :initarg :pass)
-      (ext-lang-map :accessor ext-lang-map
+      (ext-lang-map :accessor oj-ext-lang-map
                     :initarg :ext-lang-map)
-      (url :accessor url)
-      (url-problem-id :accessor url-problem-id)
-      (url-submit :accessor url-submit)
-      (url-login :accessor url-login)
-      (login-params :accessor login-params)
-      (lang-field :accessor lang-field)
+      (url :accessor oj-url)
+      (url-problem-id :accessor oj-url-problem-id)
+      (url-submit :accessor oj-url-submit)
+      (url-login :accessor oj-url-login)
+      (login-params :accessor oj-login-params)
+      (lang-field :accessor oj-lang-field)
       ))
 
 (defmethod get-sample ((c OJ))
-  (princ (format nil "Get sample ~A ..." (id c)))
+  (princ (format nil "Get sample ~A ..." (oj-id c)))
   (multiple-value-bind (body status header uri kept)
-      (s-http-client:do-http-request (format nil "~A~A~A" (url c) (url-problem-id c) (id c)))
+      (s-http-client:do-http-request (format nil "~A~A~A" (oj-url c) (oj-url-problem-id c) (oj-id c)))
     (declare (ignore status header uri kept))
     ;(format t "~&Status: ~A~%Body: ~A~%Header: ~A~%" status body header)
-    (with-open-file (stream (format nil "~A~A" (id c) sample-infile-ext)
+    (with-open-file (stream (format nil "~A~A" (oj-id c) sample-infile-ext)
                             :direction :output
                             :if-exists :supersede
                             :if-does-not-exist :create)
       (princ (sample-input c body) stream))
-    (with-open-file (stream (format nil "~A~A" (id c) sample-outfile-ext)
+    (with-open-file (stream (format nil "~A~A" (oj-id c) sample-outfile-ext)
                             :direction :output
                             :if-exists :supersede
                             :if-does-not-exist :create)
       (princ (sample-output c body) stream))))
 
 (defmethod submit ((c OJ) file)
-  (if (null file) (setq file (format nil "~A~A" (id c) default-program-ext)))
+  (if (null file) (setq file (format nil "~A~A" (oj-id c) default-program-ext)))
   (let ((ext (pathname-type file))
         (source (with-open-file (in file :if-does-not-exist :error)
                   (let ((str ""))
@@ -45,14 +45,14 @@
                             (setq str (format nil "~A~A~%" str line)))
                     str)))
         (cookie-jar (make-instance 'drakma:cookie-jar)))
-    (when (url-login c)
+    (when (oj-url-login c)
       (format t "Login ...~%")
-      (drakma:http-request (format nil "~A~A" (url c) (url-login c))
+      (drakma:http-request (format nil "~A~A" (oj-url c) (oj-url-login c))
        :method :post
-       :parameters (login-params c)
+       :parameters (oj-login-params c)
        :cookie-jar cookie-jar))
-    (format t "Submit as ~A code ...~%" (cdr (assoc ext (ext-lang-map c) :test 'string=)))
-    (drakma:http-request (format nil "~A~A" (url c) (url-submit c))
+    (format t "Submit as ~A code ...~%" (cdr (assoc ext (oj-ext-lang-map c) :test 'string=)))
+    (drakma:http-request (format nil "~A~A" (oj-url c) (oj-url-submit c))
      :method :post
      :parameters (submit-params c source ext)
      :cookie-jar cookie-jar)))
